34240be4902deee26a4374ea39a9961d90bb5b39
Nicolas Siver <nicolas@siver.im>
2015-04-30 13:19:04 +0200

    Add nconf documentation

--- app/nodebb.js
+++ app/nodebb.js
@@ -13,6 +13,24 @@
         topics       : NodeBB.require('./src/topics'),
         user         : NodeBB.require('./src/user'),
 
+        /**
+         * List is incomplete
+         *
+         * base_dir: '/path/to/NodeBB',
+         * themes_path: '/path/to/NodeBB/node_modules',
+         * views_dir: '/path/to/NodeBB/public/templates',
+         * version: 'NodeBB Version',
+         * url: 'http://localhost:4567',
+         * core_templates_path: '/path/to/NodeBB/src/views',
+         * base_templates_path: '/path/to/NodeBB/node_modules/nodebb-theme-vanilla/templates',
+         * upload_path: '/public/uploads',
+         * relative_path: '',
+         * port: '4567',
+         * upload_url: '/uploads/',
+         * theme_templates_path: '/path/to/NodeBB/node_modules/nodebb-theme-lavender/templates',
+         * theme_config: '/path/to/NodeBB/node_modules/nodebb-theme-lavender/theme.json',
+         * NODE_ENV: 'development'
+         */
         nconf: NodeBB.require('nconf')
     };
 
