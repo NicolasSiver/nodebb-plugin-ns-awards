a6b9cd3d5e1792ad415314ebe5f113e36cb68b27
Nicolas Siver <nicolas@siver.im>
2016-01-18 21:57:57 -0500

    edit user store with simple list

--- client/acp/stores/EditUserStore.js
+++ client/acp/stores/EditUserStore.js
@@ -1,18 +1,17 @@
-var AppDispatcher      = require('../dispatcher/AppDispatcher'),
-    EventEmitter       = require('events').EventEmitter,
-    assign             = require('react/lib/Object.assign'),
-    Constants          = require('../Constants'),
-    socket             = require('socket'),
+var AppDispatcher = require('../dispatcher/AppDispatcher'),
+    assign        = require('react/lib/Object.assign'),
+    Constants     = require('../Constants'),
+    EventEmitter  = require('events').EventEmitter,
+    socket        = require('socket'),
 
-    CHANGE_EVENT       = 'change',
-    API                = {
-        SEARCH_USER: 'plugins.ns-awards.searchUser'
+    CHANGE_EVENT  = 'change',
+    API           = {
+        DELETE_GRANT: 'plugins.ns-awards.deleteGrant',
+        GET_GRANTS  : 'plugins.ns-awards.getGrantsWithAwards'
     },
-    _result            = [],
-    _resultSelectIndex = 0,
-    _selected          = [];
+    _users        = [];
 
-var SearchUsersStore = assign({}, EventEmitter.prototype, {
+var EditUserStore = assign({}, EventEmitter.prototype, {
     addChangeListener: function (listener) {
         this.on(CHANGE_EVENT, listener);
     },
@@ -21,16 +20,8 @@ var SearchUsersStore = assign({}, EventEmitter.prototype, {
         this.emit(CHANGE_EVENT);
     },
 
-    getResult: function () {
-        return _result;
-    },
-
-    getResultSelectIndex: function () {
-        return _resultSelectIndex;
-    },
-
-    getSelected: function () {
-        return _selected;
+    getUsers: function () {
+        return _users;
     },
 
     removeChangeListener: function (listener) {
@@ -40,71 +31,13 @@ var SearchUsersStore = assign({}, EventEmitter.prototype, {
 
 AppDispatcher.register(function (action) {
     switch (action.actionType) {
-        case Constants.EVENT_SEARCH_USER:
-            socket.emit(API.SEARCH_USER, {
-                username: action.request
-            }, function (error, searchResult) {
-                //Search Result will have signature: {matchCount: 1, pagination: PaginationMeta, pageCount: 1, timing: "0.01", users: Array[N]}
-                _result = searchResult.users;
-                // Adjust latest selection
-                if (_resultSelectIndex >= _result.length) {
-                    _resultSelectIndex = _result.length - 1;
-                }
-                SearchUsersStore.emitChange();
-            });
-            break;
-        case Constants.EVENT_CLEAR_SEARCH_RESULT:
-            clear();
-            break;
-        case Constants.EVENT_OFFSET_USER_FROM_SEARCH_ON:
-            _resultSelectIndex += action.offset;
-            if (_resultSelectIndex < 0) {
-                if (_result.length > 0) {
-                    _resultSelectIndex = _result.length - 1;
-                } else {
-                    _resultSelectIndex = 0;
-                }
-            } else if (_resultSelectIndex >= _result.length) {
-                _resultSelectIndex = 0;
-            }
-            SearchUsersStore.emitChange();
-            break;
-        case Constants.EVENT_PICK_USER_FROM_SEARCH:
-            pickUserAt(_resultSelectIndex);
-            break;
-        case Constants.EVENT_PICK_USER_FROM_SEARCH_AT:
-            pickUserAt(action.index);
-            break;
-        case Constants.EVENT_UNPICK_USER_FROM_SEARCH:
-            _selected.splice(action.index, 1);
-            SearchUsersStore.emitChange();
-            break;
-        case Constants.EVENT_AWARD_USERS:
-            clear();
+        case Constants.EVENT_USER_DID_SELECT:
+            _users = _users.concat(action.payload.user);
+            EditUserStore.emitChange();
             break;
         default:
             return true;
     }
 });
 
-function clear() {
-    _result.length = 0;
-    _resultSelectIndex = 0;
-    _selected.length = 0;
-    SearchUsersStore.emitChange();
-}
-
-function pickUserAt(index) {
-    // Validation
-    if (index < 0 || index >= _result.length) {
-        console.error('Index for picking User is invalid, something went wrong in the system...');
-        return;
-    }
-
-    _selected.push(_result[index]);
-    _result.length = 0;
-    _resultSelectIndex = 0;
-    SearchUsersStore.emitChange();
-}
-
-module.exports = SearchUsersStore;
+module.exports = EditUserStore;
