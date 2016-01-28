eda50e8802361ee0d34775c72d7c54e0d34a67d4
Nicolas Siver <nicolas@siver.im>
2016-01-27 22:28:29 -0500

    update donation section with paypal

--- public/js/acp.js
+++ public/js/acp.js
@@ -932,19 +932,35 @@ var Donate = React.createClass({displayName: "Donate",
             React.createElement("div", null, 
                 React.createElement("p", null, "Do you like plugin? Support developer. Make a donation. Thank you in advance."), 
 
-                React.createElement("div", {className: "input-group"}, 
-                    React.createElement("input", {
-                        type: "text", 
-                        className: "form-control", 
-                        placeholder: "Amount", 
-                        valueLink: this.linkState('amount')}), 
+                React.createElement("div", {className: "row"}, 
+                    React.createElement("div", {className: "col-md-6"}, 
+                        React.createElement("h5", null, "Stripe:"), 
+
+                        React.createElement("div", {className: "input-group"}, 
+                            React.createElement("input", {
+                                type: "text", 
+                                className: "form-control", 
+                                placeholder: "Amount", 
+                                valueLink: this.linkState('amount')}), 
                         React.createElement("span", {className: "input-group-btn"}, 
                             React.createElement("button", {
                                 className: "btn btn-primary", 
                                 type: "button", 
-                                onClick: this._donateDidClick}, "Donate ", React.createElement("small", null, "via Stripe")
+                                onClick: this._donateDidClick}, "Donate via Stripe"
                             )
                         )
+                        )
+                    ), 
+                    React.createElement("div", {className: "col-md-6"}, 
+                        React.createElement("h5", null, "PayPal:"), 
+
+                        React.createElement("form", {action: "https://www.paypal.com/cgi-bin/webscr", method: "post", target: "_top"}, 
+                            React.createElement("input", {type: "hidden", name: "cmd", value: "_s-xclick"}), 
+                            React.createElement("input", {type: "hidden", name: "encrypted", value: "-----BEGIN PKCS7-----MIIHTwYJKoZIhvcNAQcEoIIHQDCCBzwCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYADFMHuCYxOg7gfS/mhJGGnN7ioPOL/Riz96CvvxFCv5zS/24VOd8zctu/k8NopEhSUeSv50LDTHh1V4XXeSOKo2+jt0AKHKuLYGXH/6cgeynYEobE65PSMtMOrDTGumtooQyBGHWLQbhoc9psGAh7lu+HFoEQuRTQRpLfnX/yPzTELMAkGBSsOAwIaBQAwgcwGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQI1PzgZLAB29yAgagrpAqgg2zfAqW2OYV6sUn8pXORe8ul5e82C73dEqJUc+Wog33ARnXpMsFQKWbPmzBbvXqa8hl+5umr022L3dIzn1gthfcKR78sI3+607lwwsfj21pryqnfqiaF8M/LRIYfRTjBRkSyZ1BnO54rq9EEGhfvc/QqOVUdO5FxRSi7/IG+WfrisHcJBFF+/Zeu1N3KtDUVgVybuP/QV/5i396H+mcs3hCnmf2gggOHMIIDgzCCAuygAwIBAgIBADANBgkqhkiG9w0BAQUFADCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wHhcNMDQwMjEzMTAxMzE1WhcNMzUwMjEzMTAxMzE1WjCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAMFHTt38RMxLXJyO2SmS+Ndl72T7oKJ4u4uw+6awntALWh03PewmIJuzbALScsTS4sZoS1fKciBGoh11gIfHzylvkdNe/hJl66/RGqrj5rFb08sAABNTzDTiqqNpJeBsYs/c2aiGozptX2RlnBktH+SUNpAajW724Nv2Wvhif6sFAgMBAAGjge4wgeswHQYDVR0OBBYEFJaffLvGbxe9WT9S1wob7BDWZJRrMIG7BgNVHSMEgbMwgbCAFJaffLvGbxe9WT9S1wob7BDWZJRroYGUpIGRMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbYIBADAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBQUAA4GBAIFfOlaagFrl71+jq6OKidbWFSE+Q4FqROvdgIONth+8kSK//Y/4ihuE4Ymvzn5ceE3S/iBSQQMjyvb+s2TWbQYDwcp129OPIbD9epdr4tJOUNiSojw7BHwYRiPh58S1xGlFgHFXwrEBb3dgNbMUa+u4qectsMAXpVHnD9wIyfmHMYIBmjCCAZYCAQEwgZQwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tAgEAMAkGBSsOAwIaBQCgXTAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0xNjAxMjgwMzIyNDlaMCMGCSqGSIb3DQEJBDEWBBS+nojMcS6t84UJDi6zge5SEpM9zTANBgkqhkiG9w0BAQEFAASBgH72sVSHH1kjiffADAlTZJVipTDtVwUOsNuJhF95rBK/dG/4EdwASUYtMSSQBAUyGMXZysqwUVtv24TtwgBN9lhkDAdRNmpCqjSrbiws5Ll5ApijvyKX9IpcZvZSGPTXC+va6RC22IeeMosNusfSQSBoKmBUaDWzOMltJa/rNxZz-----END PKCS7-----"}), 
+                            React.createElement("input", {type: "image", src: "https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif", border: "0", name: "submit", alt: "PayPal - The safer, easier way to pay online!"}), 
+                            React.createElement("img", {alt: "", border: "0", src: "https://www.paypalobjects.com/en_US/i/scr/pixel.gif", width: "1", height: "1"})
+                        )
+                    )
                 )
             )
         );
