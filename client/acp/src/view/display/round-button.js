bdc249ef5e243b52b7c6ae173878ee39830ce653
Nicolas Siver <nicolas@siver.im>
2017-03-25 19:35:10 -0400

    add roles to round button

--- client/acp/src/view/display/round-button.js
+++ client/acp/src/view/display/round-button.js
@@ -1,10 +1,16 @@
+import classNames from 'classnames';
 import React from 'react';
 
 export default class RoundButton extends React.Component {
     render() {
+        let backgroundClass = classNames('round-button__background', 'fa', 'fa-circle', 'fa-stack-2x', {
+            'round-button__background--active' : this.props.role === 'active',
+            'round-button__background--danger' : this.props.role === 'danger',
+            'round-button__background--success': this.props.role === 'success'
+        });
         return (
             <div className="round-button fa-stack">
-                <i className="round-button__background fa fa-circle fa-stack-2x"></i>
+                <i className={backgroundClass}></i>
                 <i className={`round-button__icon fa ${this.props.icon} fa-stack-1x fa-inverse`}></i>
             </div>
         );
@@ -12,5 +18,6 @@ export default class RoundButton extends React.Component {
 }
 
 RoundButton.PropTypes = {
-    icon: React.PropTypes.string.isRequired
+    icon: React.PropTypes.string.isRequired,
+    role: React.PropTypes.string.isRequired
 };
