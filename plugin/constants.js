8210339ed3a1405d3504bc3f0a0d5f0d980b5aa2
Nicolas Siver <nicolas@siver.im>
2017-03-26 11:32:09 -0400

    move path values to constants

--- plugin/constants.js
+++ plugin/constants.js
@@ -1,7 +1,10 @@
 module.exports = Object.freeze({
+    'API_PATH'            : '/api',
     'GLOBAL_AWARD_COUNTER': 'nextNsAwardId',
     'GLOBAL_GRANT_COUNTER': 'nextNsAwardGrantId',
+    'IMAGE_SERVICE_PATH'  : '/images',
     'NAMESPACE'           : 'ns:awards',
+    'PLUGIN_PATH'         : '/admin/plugins/awards',
     'SOCKETS'             : 'ns-awards',
     'UPLOAD_DIR'          : 'awards'
 });
