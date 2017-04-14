ea17ecbea845a594b4481347489acc2b2c6cdf80
Nicolas Siver <nicolas@siver.im>
2017-04-14 00:06:22 -0400

    add sizes to avatar

--- client/acp/src/view/display/avatar.js
+++ client/acp/src/view/display/avatar.js
@@ -1,20 +1,23 @@
+import classNames from 'classnames';
 import React from 'react';
 
 export default class Avatar extends React.Component {
 
     render() {
+        let avatarClass = classNames('aws-avatar', 'aws-avatar--' + this.props.size);
+        let iconClass = classNames('aws-avatar__icon', 'aws-avatar__icon--' + this.props.size);
         let content;
 
         if (this.props.user.picture) {
             content = <img className="aws-avatar__image img-responsive" src={this.props.user.picture}/>;
         } else {
-            content = <div className="aws-avatar__icon" style={{backgroundColor: this.props.user['icon:bgColor']}}>
+            content = <div className={iconClass} style={{backgroundColor: this.props.user['icon:bgColor']}}>
                 {this.props.user['icon:text']}
             </div>;
         }
 
         return (
-            <div className="aws-avatar">
+            <div className={avatarClass}>
                 {content}
             </div>
         );
@@ -22,6 +25,11 @@ export default class Avatar extends React.Component {
 
 }
 
+Avatar.defaultProps = {
+    size: 'normal'
+};
+
 Avatar.propTypes = {
+    size: React.PropTypes.string,
     user: React.PropTypes.object.isRequired
 };
