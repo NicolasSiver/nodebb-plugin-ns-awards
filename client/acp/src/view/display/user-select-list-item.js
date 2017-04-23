5199ce302bfe1204073bfecf32d1d1fa830a1f80
Nicolas Siver <nicolas@siver.im>
2017-04-22 22:40:24 -0400

    reuse user details for rewarding

--- client/acp/src/view/display/user-select-list-item.js
+++ client/acp/src/view/display/user-select-list-item.js
@@ -3,22 +3,15 @@ import React from 'react';
 
 import Avatar from './avatar';
 import RoundButton from './round-button';
+import UserDetails from './user-details';
 
 export default class UserSelectListItem extends React.Component {
     render() {
-        let {username, lastonlineISO, postcount, reputation, joindateISO} = this.props.user;
-        let {makeNumberHumanReadable} = window.utils;
-        let {timeago} = window.$;
-
         return (
             <div className="user-select-list-item">
                 <Avatar size="big" user={this.props.user}/>
                 <div className="user-select-list-item__details">
-                    <div>{username}</div>
-                    <div className="user-select-list-item__info">posts: {makeNumberHumanReadable(postcount)}</div>
-                    <div className="user-select-list-item__info">reputation: {makeNumberHumanReadable(reputation)}</div>
-                    <div className="user-select-list-item__info">joined: {timeago(Date.parse(joindateISO))}</div>
-                    <div className="user-select-list-item__info">last online: {timeago(Date.parse(lastonlineISO))}</div>
+                    <UserDetails user={this.props.user}/>
                 </div>
                 <RoundButton
                     icon="fa-times"
